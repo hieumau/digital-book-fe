508f1482334071b7ad688672e8b9e786ceb9107c
mauhieu12 <mauhieu12@gmail.com>
2022-09-24 01:24:24 +0700

    init product with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CreateBookComponent} from "./create-book/create-book.component";
+import {IndexComponent} from "./index/index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: 'create-book',
+    pathMatch: 'full',
+    component: CreateBookComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { SearchBookComponent } from './search-book/search-book.component';
+import { IndexComponent } from './index/index.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBookComponent,
+    SearchBookComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
