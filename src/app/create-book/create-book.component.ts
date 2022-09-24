569bae7aaa03cbfa1a249febcaf9ea6af5feae65
mauhieu12 <mauhieu12@gmail.com>
2022-09-24 21:29:32 +0700

    done login function

--- src/app/create-book/create-book.component.ts
+++ src/app/create-book/create-book.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 import {Author} from "../model/author.model";
 import {Book} from "../model/book.model";
 import {Category} from "../model/category.model";
@@ -17,28 +18,30 @@ export class CreateBookComponent implements OnInit {
   categories: Category[] = Object.values(Category)
 
   constructor(private bookService: BookService,
+              private router: Router,
               private notificationService: NotificationService) {
 
   }
 
   ngOnInit(): void {
-    this.book.active = true
-    this.book.content = 'book content'
-    this.book.logo = 'logo 1'
-    this.book.publisher = 'publisher 1'
-    this.book.price = 10000
-    this.book.title = 'book 1'
-    this.book.author = new Author()
-    this.book.author.name = 'Author 2'
-    this.book.category = Category.AutoBiography
-    this.book.publishedDate = new Date()
+    // this.book.active = true
+    // this.book.content = 'book content'
+    // this.book.logo = 'logo 1'
+    // this.book.publisher = 'publisher 1'
+    // this.book.price = 10000
+    // this.book.title = 'book 1'
+    // this.book.author = new Author()
+    // this.book.author.name = 'Author 2'
+    // this.book.category = Category.AutoBiography
+    // this.book.publishedDate = new Date()
   }
 
   save() {
     this.bookService.save(this.book).subscribe(value => {
       this.notificationService.success("Create book: " + value.title + ' successfully')
+      this.router.navigate(['search-book'])
     }, error => {
-
+      this.notificationService.success("Something went wrong!")
     })
   }
 }
